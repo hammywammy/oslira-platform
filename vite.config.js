2337d5c8af85ad55d679bce8f0a1a9f9b3b73ca0
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-28 15:50:57 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        dashboard: resolve(__dirname, 'dashboard.html'),
-        onboarding: resolve(__dirname, 'onboarding.html')
+        main: resolve(__dirname, 'src/pages/www/index.html'),
+        dashboard: resolve(__dirname, 'src/pages/app/dashboard/index.html'),
+        onboarding: resolve(__dirname, 'src/pages/app/onboarding/index.html')
       }
     }
   }
