73e5b9333acce7e2e37966ce686eb1ab833bf7b3
hammywammy <73197193+hammywammy@users.noreply.github.com>
2025-10-28 16:04:19 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,16 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    target: 'esnext',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/pages/www/index.html'),
         dashboard: resolve(__dirname, 'src/pages/app/dashboard/index.html'),
         onboarding: resolve(__dirname, 'src/pages/app/onboarding/index.html')
       },
-      external: [
-        /^\/core\//,
-        /^\/src\//,
-        /^@\//
-      ]
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
     }
   }
 });
